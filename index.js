0f5797d8b865f24e17d77e3199e048f67006911b
Moises Rodrigues Martins <moises.martins@focus.corp>
2025-08-21 13:45:09 -0300

    Força a atualização no token

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
+const mysql = require('mysql2/promise');
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -30,8 +31,10 @@ const pool = await mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  socketPath: process.env.INSTANCE_CONNECTION_NAME ? `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}` : undefined,
-  host: process.env.INSTANCE_CONNECTION_NAME ? undefined : process.env.DB_HOST,
+  // Use socketPath quando INSTANCE_CONNECTION_NAME estiver definido (ambiente de produção)
+  ...(process.env.INSTANCE_CONNECTION_NAME
+    ? { socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}` }
+    : { host: process.env.DB_HOST }) // Caso esteja rodando localmente
 });
 
 // --- MUDANÇA: CONFIGURAÇÃO DO GOOGLE VISION CLIENT ---
